864274be80dcb1702f5bdce0967bea5b3fa3cc77
fayvori <ignat.belousov2005@gmail.com>
2022-06-24 03:15:57 +0300

    Fix port issue

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
