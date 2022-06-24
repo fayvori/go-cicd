1dd747a61fc14e36db10ca0c8787f9dd03b0c8d3
fayvori <ignat.belousov2005@gmail.com>
2022-06-24 03:19:50 +0300

    Replace message

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello world!! I'm building and serving from pipeline. It's very cool!")
 	})
 
 	app.Listen(":" + port)
